372282eed6dc2d801c1669af4b7c937a8fea7665
Tejas Gupta <tejas.gupta@neosoftmail.com>
2025-01-14 16:19:22 +0530

    Initial Commit.

---
+++ jest.setup.ts
@@ -0,0 +1,11 @@
+import '@testing-library/jest-native/extend-expect';
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: jest.fn(() => ({
+      navigate: jest.fn(),
+    })),
+  };
+});
